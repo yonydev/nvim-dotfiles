d8c7e72cc6a30fb9946923f9001cb9cd051b790f
Yonatan Rojas <yonatan.rojas@crehana.com>
2023-05-24 12:13:37 -0600

    feat: updated config

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
 if vim.g.vscode then
   vim.cmd([[source $HOME/.config/nvim/vscode/settings.vim]])
   -- Optional plugin
   vim.cmd([[source $HOME/.config/nvim/vscode/easy-motion-config.vim]])
+else
+  -- bootstrap lazy.nvim, LazyVim and your plugins
+  require("config.lazy")
 end
