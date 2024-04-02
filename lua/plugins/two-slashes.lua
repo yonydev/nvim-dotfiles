b7f9def99daea57b60048f6b76db507d4e659b7a
Yonatan Rojas <yonatan.rojas@crehana.com>
2024-04-02 11:33:00 -0600

    refactor: added two-slashes plugin, removed plugins for data science, delte alpha

---
+++ lua/plugins/two-slashes.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tsserver = {
+          on_attach = function(client, buffer)
+            require("twoslash-queries").attach(client, buffer)
+          end,
+        },
+      },
+    },
+    dependencies = { "marilari88/twoslash-queries.nvim" },
+  },
+  -- {
+  --   "marilari88/twoslash-queries.nvim",
+  --   event = "LspAttach",
+  --   keys = { "<C-kk>", "<cmd>TwoslashQueriesInspect<cr>", mode = { "n" }, desc = "Inspect twoslash queries" },
+  --   config = function()
+  --     require("lazyvim.util.lsp").on_attach(function(client, buffer)
+  --       require("twoslash-queries").attach(client, buffer)
+  --     end)
+  --   end,
+  -- },
+}
